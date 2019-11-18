ff047917691282e313c6a00bea86f0b3a6b591e8
KangliMalorie <kangliwu@hsph.harvard.edu>
2019-11-18 17:36:48 -0500

    completed test_var

--- easy_diff/test_var.py
+++ easy_diff/test_var.py
@@ -43,6 +43,7 @@ def test_real_pow():
 	x = Var(3, np.array([1,0]))
 	y = 2
 	assert x**y == Var(9, np.array([6, 0]))
+	assert y**x == Var(8, np.array([pytest.approx(5.54517744), pytest.approx(0.)]))
 
 def test_truediv():
 	x = Var(3, np.array([1,0]))
@@ -53,6 +54,11 @@ def test_rtruediv():
 	x = Var(3, np.array([1,0]))
 	y = 2
 	assert y/x == Var(pytest.approx(0.6666666666666666), np.array([pytest.approx(-0.22222222), pytest.approx(-0.)]))
+
+def test_div():
+	x = Var(3, np.array([1,0]))
+	y = 2
+	assert x/y == Var(1.5, np.array([0.5, 0.]))
 
 def test_neg():
 	x = Var(3, np.array([1,0]))
@@ -77,27 +83,45 @@ def test_notequal():
 	assert (x!=y) == True
 
 def test_log():
-	x = Var(3, np.array([1,0]))
-	assert Var.log(x) == Var(pytest.approx(1.0986122886681098), np.array([0.33333333])) 
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.log(x) == Var(pytest.approx(1.0986122886681098), np.array([pytest.approx(0.33333333)]))
+	assert Var.log(y) == np.log(y)
+
+def test_logk():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.logk(x,3.0) == Var(pytest.approx(1.0), np.array([pytest.approx(0.30341308)]))
+	assert Var.logk(y,3.0) == np.log(y)/ np.log(3.0)
+
+def test_exp():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.exp(x) == Var(pytest.approx(20.085536923187668), np.array([pytest.approx(20.08553692)]))
+	assert Var.exp(y) == np.exp(y)
+
+def test_sqrt():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.sqrt(x) == Var(pytest.approx(1.7320508075688772), np.array([pytest.approx(0.28867513)]))
+	assert Var.sqrt(y) == np.sqrt(y)
+
+def test_sin():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.sin(x) == Var(pytest.approx(0.1411200080598672), np.array([pytest.approx(-0.9899925)]))
+	assert Var.sin(y) == np.sin(y)
+
+def test_cos():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.cos(x) == Var(pytest.approx(-0.9899924966004454), np.array([pytest.approx(-0.14112001)]))
+	assert Var.cos(y) == np.cos(y)
+
+def test_tan():
+	x = Var(3, np.array([1]))
+	y = 2
+	assert Var.tan(x) == Var(pytest.approx(-0.1425465430742778), np.array([pytest.approx(1.02031952)]))
+	assert Var.tan(y) == np.tan(y)
 
 
-
-
-# def test_quadroots_types():
-#     with pytest.raises(TypeError):
-#         roots.quad_roots("", "green", "hi")
-
-# def test_quadroots_zerocoeff():
-#     with pytest.raises(ValueError):
-#         roots.quad_roots(a=0.0)
-
-# def test_linearoots_result():
-#     assert roots.linear_roots(2.0, -3.0) == 1.5
-
-# def test_linearroots_types():
-#     with pytest.raises(TypeError):
-#         roots.linear_roots("ocean", 6.0)
-
-# def test_linearroots_zerocoeff():
-#     with pytest.raises(ValueError):
-#         roots.linear_roots(a=0.0)
