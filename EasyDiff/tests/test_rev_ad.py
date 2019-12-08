18a99414f2a5dd7d062721f8f4f9757fb311819a
KangliMalorie <kangliwu@hsph.harvard.edu>
2019-12-07 21:35:08 -0500

    added test ad for reverse mode

--- EasyDiff/tests/test_rev_ad.py
+++ EasyDiff/tests/test_rev_ad.py
@@ -3,25 +3,29 @@ import numpy as np
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
+from rev_var import Rev_Var
 from var import Var
 from ad import AD
+from ad import AD_Mode
 
-x = Var(3, np.array([1,0]))
-y = Var(2, np.array([0,1]))
+# x = Rev_Var(3, np.array([1,0]))
+# y = Rev_Var(2, np.array([0,1]))
 
 def test_single_variable():
-	ad = AD(np.array([2]), np.array([1]))
-	f1 = lambda x: Var.log(x) ** 2
+	ad = AD(np.array([2]), np.array([1]),AD_Mode.REVERSE)
+	f1 = lambda x: Rev_Var.log(x) ** 2
 	assert AD.auto_diff(self=ad,func=f1) == Var(pytest.approx(0.4804530139182014),np.array([pytest.approx(0.69314718)]))
 
 def test_two_variables():
-	ad = AD(np.array([2, 2]), np.array([1, 1]))
-	f1 = lambda x, y: Var.log(x) ** Var.sin(y)
+	ad = AD(np.array([2, 2]), np.array([1, 1]), AD_Mode.REVERSE)
+	f1 = lambda x, y: Rev_Var.log(x) ** Rev_Var.sin(y)
+	assert ad.vars[0].value == 2
+	assert ad.vars[1].value == 2
 	assert AD.auto_diff(self=ad,func=f1) == Var(pytest.approx(0.7165772257590739),np.array([pytest.approx(0.47001694),pytest.approx(0.10929465)]))
 
 def test_jac_matrix():
-	f1 = lambda x, y: Var.log(x) ** Var.sin(y)
-	f2 = lambda x, y: Var.sqrt(x) / y
-	ad = AD(np.array([4.12, 5.13]), np.array([1, 1]))
+	f1 = lambda x, y: Rev_Var.log(x) ** Rev_Var.sin(y)
+	f2 = lambda x, y: Rev_Var.sqrt(x) / y
+	ad = AD(np.array([4.12, 5.13]), np.array([1, 1]), AD_Mode.REVERSE)
 	assert np.array_equal(ad.jac_matrix([f1, f2]), np.array([[pytest.approx(-0.11403015), pytest.approx(0.10263124)], [pytest.approx(0.048018), pytest.approx(-0.07712832)]]))
         
