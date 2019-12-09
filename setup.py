9d570f526ba86119ebbde50ff138d9b7247847af
KangliMalorie <kangliwu@hsph.harvard.edu>
2019-12-08 19:09:31 -0500

    adding license and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CrackAD", # Replace with your own username
+    version="0.0.1",
+    author="CS207 Group18",
+    author_email="kangliwu@hsph.harvard.edu",
+    description="An auto differentiation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrackAD/cs207-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
